2b6b698b95a68547b52c85184b6b0c51ad2b40ae
Debora Urrutia <deboraut@gmail.com>
2018-10-15 20:08:24 -0400

    Add files via upload

---
+++ WebAnimal/All Files/dbheader.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<!-- S. Miller -->
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title><?php echo $pagetitle; ?></title>
+		<link href="/~cpais47368/dbStyle.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li><a href="/~cpais47368/">Home</a></li>
+				<li><a href="/~cpais47368/dbview.php">View</a></li>
+				<li><a href="/~cpais47368/dblogout.php">Logout</a></li>
+			</ul>
+		</nav>
+		<section>
---
+++ WebAnimal/All Files/dbview.php
@@ -0,0 +1,67 @@
+<?php 
+
+//BEGIN - ON EVERY PAGE TOP
+$pagetitle = "View";
+//require_once("adminchk.php");
+
+require_once("dbpwd.php"); //Creates a $db mysqli object and connects to DB
+
+require_once("dbheader.php");
+
+//END - ON EVERY PAGE TOP
+
+
+/* Select queries return a resultset */
+
+$sql = sprintf("
+	SELECT Picture,Species,Life_Span,Description,Price
+	FROM Animal 
+	
+	ORDER BY %s
+",'Pet_ID');
+
+if ($result = $db->query($sql)) {
+	if ( $result->num_rows > 0 ) {
+		?>
+		<span class="productLine">
+			
+			<?php
+			while($row = $result->fetch_assoc()) {
+				printf("<table class=\"table\">
+        <thead>
+            <tr>
+                <th class=\"mainHeading\" colspan=4> %s </th>
+                
+            </tr>
+        </thead>
+        <tbody>
+		
+            <tr>
+                <td class=\"heading\">Life Expectancy</td>
+                <td class=\"data\" colspan=2> %s  </td>
+				<td class=\"data\" rowspan=4><img src=\" %s\" height=\"100\" width=\"100\" ></td>
+            </tr>
+            <tr>
+                <td class=\"heading\"><input id=\"purchaseBtn\" type=\"image\" src=\"purchasebutton.png\"></td>
+                <td class=\"data\" colspan=2>$   %.2f </td>
+            </tr>
+		
+        </tbody>
+    </table>",$row['Species'],$row['Life_Span'],$row['Picture'],$row['Price']);
+				
+			}
+			?>
+		</span>
+		<?php
+	}
+	/* free result set */
+	mysqli_free_result($result);
+} else {
+	outputDBError($db);
+}
+$db->close();
+
+//BEGIN - ON EVERY PAGE BOTTOM
+require_once("dbfooter.php");
+//END - ON EVERY PAGE BOTTOM
+?>
