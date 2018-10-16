b826be08940d079a73e26a25a94c6a583e04430b
Debora Urrutia <deboraut@gmail.com>
2018-10-15 20:09:55 -0400

    Add files via upload

---
+++ dbheader.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<!-- S. Miller -->
+<html>
+	<head>
+		<meta charset = "utf-8">
+		<title><?php echo $pagetitle; ?></title>
+		<link href="/~cpais47368/dbStyle.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<nav>
+		<!--
+			<ul>
+				<li><a href="/~cpais47368/">Home</a></li>
+				<li><a href="/~cpais47368/dbview.php">View</a></li>
+				<li><a href="/~cpais47368/dblogout.php">Logout</a></li>
+			</ul>
+		-->
+				<ul>
+					<li><a class="active" href="#home">Home</a></li>
+					<li><a href="#news">News</a></li>
+					<li><a href="#contact">Contact</a></li>
+					<li><a href="#about">About</a></li>
+				</ul>
+				<span id ="companyName">
+				<span class="whiteText">PET ENTER</span><span class="redText">PRISES</span>
+				</span>
+				
+		</nav>
+		<section>
